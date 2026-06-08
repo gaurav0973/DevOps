a251016d2ba8f308f9227a7684f8b41edf638069
gaurav0973 <gauravKmaurya09@gmail.com>
2026-06-08 17:40:58 +0530

    struggkling with isue

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 8000;
 
 app.get('/', (req, res) => {
     res.json({
-        message: 'First CI/CD pipeline testing auto deployment',
+        message: 'First CI/CD pipeline testing auto deployment 🪈',
         status: 'success'
     });
 });
