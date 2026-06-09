424c1ee542a42171a3c1221b7ef8617a12fad90b
gaurav0973 <gauravKmaurya09@gmail.com>
2026-06-09 10:38:15 +0530

    Docker access toekn permission: read write and delete

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 8000;
 
 app.get('/', (req, res) => {
     res.json({
-        message: 'First CI/CD pipeline testing auto deployment: Docker Hub',
+        message: 'First CI/CD pipeline testing auto deployment: Docker Access token: Read, write and delete',
         status: 'success'
     });
 });
