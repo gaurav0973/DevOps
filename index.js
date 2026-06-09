f14e1c900c05bb742487a2c3161bb86e65f6deb6
gaurav0973 <gauravKmaurya09@gmail.com>
2026-06-09 13:45:09 +0530

    refactor: add knownIssue field to response message

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.get('/', (req, res) => {
     res.json({
         message: "Attached with a domain name now : devops.campuscrush.site 🐋",
         peakDetailing: "SSL certificate is generated using Caddy server and the traffic is being proxied to this API service running on port 8000.",
+        knownIssue: "Docker container in not cleaning up properly",
         status: "success",
     });
 });
