2655960466cc3cdce49c172505a941a8f98f9521
gaurav0973 <gauravKmaurya09@gmail.com>
2026-06-09 13:36:41 +0530

    refactor: update response structure to include peakDetailing information

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = 8000;
 app.get('/', (req, res) => {
     res.json({
         message: "Attached with a domain name now : devops.campuscrush.site 🐋",
+        peakDetailing: "SSL certificate is generated using Caddy server and the traffic is being proxied to this API service running on port 8000."
         status: "success",
     });
 });
