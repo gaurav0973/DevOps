914d3c78b2f657870c3dc947b532f8656f4c1144
gaurav0973 <gauravKmaurya09@gmail.com>
2026-06-10 17:48:18 +0530

    chenges

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = 8000;
 app.get('/', (req, res) => {
     res.json({
         message: "Full API service deployed successfully with Caddy server as a reverse proxy.",
-        peakDetailing: "Now I can use this repository to deploy any API service with Caddy server as a reverse proxy.",
+        peakDetailing: "Now I can use this repository to deploy any API service with Caddy server as a reverse proxy",
         status: "success",
     });
 });
